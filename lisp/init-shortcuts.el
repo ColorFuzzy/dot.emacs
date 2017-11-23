3ead90455cd4b2891a13a484f83df24ccdd94c5b
eks.lv <eks.lv@cootek.cn>
2017-11-23 14:45:20 +0000

    add compile function

--- lisp/init-shortcuts.el
+++ lisp/init-shortcuts.el
@@ -1,2 +1,10 @@
+(defun x/byte-compile-init-dir ()
+  "Byte-compile all your dotfiles."
+  (interactive)
+  (byte-recompile-directory user-emacs-directory 0)
+  (dolist (elc '("lisp/init-elpa.elc" "lisp/init-shortcuts.elc"))
+    (delete-file (expand-file-name elc user-emacs-directory))))
+
+(define-key global-map (kbd "C-c e c") 'x/byte-compile-init-dir)
 
 (provide 'init-shortcuts)
