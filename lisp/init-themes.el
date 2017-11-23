dc3f77b0e9e94b7786773e7f6b442ef9d88acee1
eks.lv <eks.lv@cootek.cn>
2017-11-23 15:56:41 +0000

    ...

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -8,7 +8,7 @@
 (x/require-package 'monokai-theme)
 (load-theme 'monokai t)
 
-(setq-default left-margin-width 1 right-margin-width 1)
+(setq-default left-margin-width 1 right-margin-width 0)
 
 (set-face-background 'mode-line "#303030")
 (set-face-background 'mode-line-inactive "#121212")
