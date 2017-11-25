d1166f7bb784150c1d4bb27637e0c6a3f7ebe9bd
eks.lv <eks.lv@cootek.cn>
2017-11-25 02:28:09 +0000

    git restclient to company

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -6,4 +6,8 @@
 (setq company-minimum-prefix-length 1)
 (setq company-show-numbers t)
 
+(x/require-package 'company-restclient)
+(with-eval-after-load 'global-company-mode
+  (add-to-list 'company-backends 'company-restclient))
+
 (provide 'init-company)
--- lisp/init-shortcuts.el
+++ lisp/init-shortcuts.el
@@ -55,6 +55,7 @@
 (define-key global-map (kbd "C-x b") 'helm-mini)
 (define-key global-map (kbd "C-x C-b") 'helm-buffers-list)
 (define-key global-map (kbd "C-x C-f") 'helm-find-files)
+(define-key global-map (kbd "C-x C-r") 'helm-recentf)
 (define-key global-map (kbd "C-x r b") 'helm-bookmarks)
 (define-key global-map (kbd "C-z") 'evil-exit-emacs-state)
 (define-key global-map (kbd "M-x") 'helm-M-x)
