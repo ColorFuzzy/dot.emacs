a09a24b5fa802e2f05b1bbc9e878728b2ba4bc15
Color Fuzzy <color.fuzzy@gmail.com>
2017-11-27 21:02:53 +0800

    add color

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -64,11 +64,11 @@
      ;; highlight-symbol
      '(highlight-symbol-face ((t (:underline nil :background "#4f5769"))))
      ;; org-mode
-     `(org-level-1 ((t (:bold t :foreground "dodger blue" :height 1.3))))
-     `(org-level-2 ((t (:bold t :foreground "#edd400" :height 1.2))))
+     `(org-level-1 ((t (:bold t :foreground "dodger blue" :height 1.1))))
+     `(org-level-2 ((t (:bold t :foreground "#edd400" :height 1.1))))
      `(org-level-3 ((t (:bold t :foreground "#6ac214" :height 1.1))))
-     `(org-level-4 ((t (:bold nil :foreground "#30e8d2" :height 1.0))))
-     `(org-level-5 ((t (:bold nil :foreground "#e539a0" :height 0.9))))
+     `(org-level-4 ((t (:bold nil :foreground "#30e8d2" :height 1.1))))
+     `(org-level-5 ((t (:bold nil :foreground "#e539a0" :height 1.1))))
      )
   (custom-set-faces
    ;; show paren match
